01cbc6a661f32a45a673a5a5682a9ba98f7f213d
Michał Mielczarek <>
2018-09-05 19:22:09 +0200

    Init commit

---
+++ src/main/java/pl/ymzs/springfirstweb/SpringfirstwebApplication.java
@@ -0,0 +1,12 @@
+package pl.ymzs.springfirstweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfirstwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfirstwebApplication.class, args);
+	}
+}
